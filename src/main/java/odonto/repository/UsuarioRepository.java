e7acdeccadff338cafb69623f556f916cede6d7e
wendellsilva1 <wendell.silva@dbserver.com.br>
2025-03-08 21:34:13 -0300

    adicionando cros origin, webconfig e endpoint login

--- src/main/java/odonto/repository/UsuarioRepository.java
+++ src/main/java/odonto/repository/UsuarioRepository.java
@@ -5,5 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import odonto.model.Usuario;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
 }
